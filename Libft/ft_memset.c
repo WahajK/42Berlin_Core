1117d5d90ab91ab74ab78c1e1ea5e21098d30369
WahajK <wahajkhan788@gmail.com>
2024-11-21 01:27:52 +0100

    Edited Makefile and fixed some problems

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -12,12 +12,10 @@
 
 #include <unistd.h>
 
-void	*memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 
-	if (!s)
-		return (NULL);
 	i = 0;
 	while (i < n)
 		*(unsigned char *) (s + (i++)) = (unsigned char) c;
--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -19,25 +19,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] && s2[i] && i < n)
 	{
-		if (s1[i] - s2[i] != 0)
-			return (s1[i] - s2[i]);
+		if ((unsigned char) s1[i] - (unsigned char) s2[i] != 0)
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i == n)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-/*
-#include <stdio.h>
-
-int main(void)
-{
-	char str1[] = "Meow";
-	char str2[] = "Mellyyyyyyy";
-	unsigned int n = 3;
-
-	int result = ft_strncmp(str1, str2, n);
-	printf("Comparison result: %d\n", result);
-
-	return 0;
-}*/
